7d0b052d08f821e7549e7db19f36e6222c965df6
Puzzookie <100958131+Puzzookie@users.noreply.github.com>
2024-11-16 20:14:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ export default async ({ req, res, log, error }) => {
     if(event === "users." + userId + ".create")
     {      
         
-        const response = await model.generateContent("Your response to this question will be recorded as a bool value, so it's imperative that you only respond with true or false and nothing else. Is the sky blue?");
+        const response = await model.generateContent("Your response to this question will be recorded as a bool value, so it's imperative that you only respond with true or false and nothing else. Does 2 + 2 = 4?");
         log(response);
         let text = response.response.candidates[0].content.parts[0].text;
 
