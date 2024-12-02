d305c7680f8d77878e4356f689f08df73da169c6
Puzzookie <100958131+Puzzookie@users.noreply.github.com>
2024-12-02 11:20:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ export default async ({ req, res, log, error }) => {
   const client = new Client()
     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(req.headers['x-appwrite-key'] ?? '');
+    .setKey(process.env.APPWRITE_API_KEY);
   
   const account = new Account(client);
   const db = new Databases(client);
