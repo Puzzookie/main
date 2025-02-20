9a5def956079aea05b52129fa3c9ba93164c4eb7
Puzzookie <100958131+Puzzookie@users.noreply.github.com>
2025-02-20 13:49:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ export default async ({ req, res, log, error }) => {
   {
     try {
         //req.method
-        log(req);
+        log(req.headers);
         return res.json({ ok: true }, 200, {
           'Access-Control-Allow-Origin': 'https://9000-idx-lds-1737864063978.cluster-aj77uug3sjd4iut4ev6a4jbtf2.cloudworkstations.dev',
         });
