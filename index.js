c3966a70138c5523a313296f68ca72afa94a71af
Puzzookie <100958131+Puzzookie@users.noreply.github.com>
2025-03-11 13:09:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,17 +4,7 @@ export default async ({ req, res, log, error }) => {
 
   if (req.path === "/") 
   {
-    try {
-        return res.json({ status: req.headers }, 200, {
-          'Access-Control-Allow-Origin': 'https://9000-idx-lds-1737864063978.cluster-aj77uug3sjd4iut4ev6a4jbtf2.cloudworkstations.dev',
-        });     
-    } 
-    catch (err) {
-         return res.json({ status: false }, 200, {
-          'Access-Control-Allow-Origin': 'https://9000-idx-lds-1737864063978.cluster-aj77uug3sjd4iut4ev6a4jbtf2.cloudworkstations.dev',
-        });     
-        error(err);
-      }
+    log(req.headers);
   }
   return res.json({ status: "other" }, 200, {
       'Access-Control-Allow-Origin': 'https://9000-idx-lds-1737864063978.cluster-aj77uug3sjd4iut4ev6a4jbtf2.cloudworkstations.dev',
