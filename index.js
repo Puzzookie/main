94b2b9ef17171a1fd27faacc48a4e830f06572ed
Puzzookie <100958131+Puzzookie@users.noreply.github.com>
2025-03-12 13:23:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ export default async ({ req, res, log, error }) => {
 
   if (req.path === "/") 
   {
+     const jwt = req.headers['x-appwrite-user-jwt'];
+      if (jwt) {
+        const client = new Client()
+            .setEndpoint('https://cloud.appwrite.io/v1')
+            .setProject('66f17bf2003d23b70795')
+            .setJWT(jwt);
+        
+        const databases = new Databases(client);
+        
+        const documents = await databases.listDocuments(
+            'db',
+            'test'
+        );
+        
+        log(documents);
+
+      } else {
+        log('No authenticated user');
+      }
+      return res.json({ status: 'ok' });
     log(req.headers);
   }
   return res.json({ status: "other" });
