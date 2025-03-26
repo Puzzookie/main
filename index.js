f1a73bafda80e792fbce3ed4c79c7102312d1a44
Puzzookie <100958131+Puzzookie@users.noreply.github.com>
2025-03-26 12:32:35 -0600

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,14 @@ export default async ({ req, res, log, error }) => {
 
     const event = req.headers['x-appwrite-event'];
     log(event);
-    log(req);
+    log(req.body.$id);
+    let sessionId = "";
     
-    const sessionId = "test";
+    if(req.body.$id)
+    {
+      sessionId = req.body.$id;
+    };
+
     //const session = req.headers[
     if(event === "users." + userId + ".create")
     {
@@ -22,18 +27,17 @@ export default async ({ req, res, log, error }) => {
     }
     else if(event === "users." + userId + ".sessions." + sessionId + ".create")
     {
-        log("New session created");
-
-       /* const sessionClient = new Client()
-           .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
-           .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-           .setSession(sessionId);
-  
-        const account = new Account(sessionClient);
-    
-        const result = await account.getSession(
-            '<SESSION_ID>'
-        );*/
+        const googleToken = req.body.providerAccessToken;
+        fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${googleToken}`, {
+          method: 'GET'
+        })
+        .then(response => response.json())
+        .then(data => {
+          log(data.picture);
+        })
+        .catch(error => {
+          error('Error fetching user info:', error);
+        });
     }
   }
   return res.json({ status: "complete" });
