70ca04f970ac3597d313a9edbf5b15c8c1a43d70
Puzzookie <100958131+Puzzookie@users.noreply.github.com>
2025-03-26 12:18:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,32 @@ export default async ({ req, res, log, error }) => {
        .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
        .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
        .setKey(req.headers['x-appwrite-key'] ?? '');
+
+    const event = req.headers['x-appwrite-event'];
+    log(event);
+    log(req);
+    
+    const sessionId = "test";
+    //const session = req.headers[
+    if(event === "users." + userId + ".create")
+    {
+        log("New user created");
+    }
+    else if(event === "users." + userId + ".sessions." + sessionId + ".create")
+    {
+        log("New session created");
+
+       /* const sessionClient = new Client()
+           .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
+           .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+           .setSession(sessionId);
   
-      const users = new Users(client);
-      
-      const result = await users.updateLabels(
-          userId,
-          [userId]
-      );
+        const account = new Account(sessionClient);
+    
+        const result = await account.getSession(
+            '<SESSION_ID>'
+        );*/
+    }
   }
-  return res.json({ status: "other" });
+  return res.json({ status: "complete" });
 };
