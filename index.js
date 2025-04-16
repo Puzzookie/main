e8789d6e7a85dd9c5d8a83f262fedb1acc40fb15
Puzzookie <100958131+Puzzookie@users.noreply.github.com>
2025-04-16 13:34:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ import { Client, Account, Databases, Users, Permission, Role, Query, ID } from '
 
 export default async ({ req, res, log, error }) => {
 
-  if (req.path === "/") 
-  {
-    const userId = req.headers['x-appwrite-user-id'];
-    const client = new Client()
-       .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
-       .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-       .setKey(req.headers['x-appwrite-key'] ?? '');
-    
-    const account = new Account(client);
-    const db = new Databases(client);
-    const users = new Users(client);
-    
-    const event = req.headers['x-appwrite-event'];
+  const userId = req.headers['x-appwrite-user-id'];
+  
+  const client = new Client()
+     .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
+     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+     .setKey(req.headers['x-appwrite-key'] ?? '');
+  
+  const account = new Account(client);
+  const users = new Users(client);
+  const db = new Databases(client);
 
-    log(req);
+  
+  if (req.path === "/") 
+  { 
+    const event = req.headers['x-appwrite-event'];
     
     if(event === "users." + userId + ".create")
     {
@@ -46,6 +46,11 @@ export default async ({ req, res, log, error }) => {
     {
         //Gemini implementation to check to see if the post is good
     }
+  }
+  else if(req.path === "/setup")
+  {
+      log(userId);
+      log(req.body);
   }
   return res.json({ status: "complete" });
 };
